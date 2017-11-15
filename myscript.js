551d2286fff60317a158399c5fd362b89df4fe6c
adhikary97 <adhikary@purdue.edu>
2017-11-15 11:16:38 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,4 @@
+var siteWidth = 1280;
+var scale = screen.width /siteWidth
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
